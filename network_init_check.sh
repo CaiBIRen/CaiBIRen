cfece601e1f15b8eda8839fbde1d14ffd87fcc87
huomengfei <huomengfei@unicloud.com>
2022-11-02 18:44:02 +0800

    配置文件完成

--- network_init_check.sh
+++ network_init_check.sh
@@ -8,22 +8,31 @@ echo -e "------------${REDCOLOR}[Red is Error]${ENDCOLOR} ; ${GREENCOLOR}[Green 
 
 CONFIGPATH="/etc/network-cvk-agent/config.json"
 
-#1、检查网络服务状态
+function funcIsExistListJsonValues() {
+    # $1 file  $2 key
+    VALUE="`awk BEGIN{RS=EOF}'{gsub(/\n/," ");print}' $1 | grep -o "$2[^]]*" | awk -F '[' '{print}'`"
+    if [ -z $value ];then
+        return 1
+    else
+        return 0
+    fi
+}
+
 function funcServiceStatus() 
 
 {
     service_name=$1
 
     if [ -z "`systemctl status $service_name | grep -w "active"`" ];then
-        echo -e "`printf "%-30s\n" $service_name`                                                  ${REDCOLOR}[Failed]${ENDCOLOR}"
+        echo -e "`printf "%-100s\n" $service_name` ${REDCOLOR}[Failed]${ENDCOLOR}"
     else
-        echo -e "`printf "%-30s\n" $service_name`                                                  ${GREENCOLOR}[OK]${ENDCOLOR}"
+        echo -e "`printf "%-100s\n" $service_name` ${GREENCOLOR}[OK]${ENDCOLOR}"
     fi
 }
 
 function funcIsEmpty(){
     if [ -z $1 ];then
-        echo -e "config.json $2 field missing,please check it                   ${REDCOLOR}[ERROR]${ENDCOLOR}"
+        echo -e "`printf "%-100s\n" config.json $2 field` ${REDCOLOR}[ERROR]${ENDCOLOR}"
         return 1
     else
         return 0
@@ -31,13 +40,34 @@ function funcIsEmpty(){
 }
 
 function funcCheckRedis(){
+    REDISMODE=`cat /etc/network-cvk-agent/config.json | grep -w mode | awk -F '"' '{print $4}'`
+    if [ $REDISMODE ="master" ];then
+        REDISINFO=`cat /etc/network-cvk-agent/config.json | grep -w address | awk -F '"' '{print $4}'`
+        PASSWORD=`cat /etc/network-cvk-agent/config.json | grep -w password | awk -F '"' '{print $4}'`
+        if [[ -z $REDISINFO ]] || [[ -z $PASSWORD ]];then
+            echo -e "`printf "%-100s\n" config.json subscriberconfig field` ${REDCOLOR}[ERROR]${ENDCOLOR}"
+            return 1
+        fi
+    else
+        if [ funcIsExistListJsonValues ${CONFIGPATH} "sentinaladdrs" = 1 ];then
+            echo -e "`printf "%-100s\n" config.json subscriberconfig field` ${REDCOLOR}[ERROR]${ENDCOLOR}"
+            return 1
+        fi
+
+
     AGENTOFFLINE=`cat /etc/network-cvk-agent/config.json | grep -w offline | awk -F '"' '{print $4}'`
     if [ AGENTOFFLINE = "true"];then
-        echo -e "please check netwrok-cvk-agent service redis status            ${REDCOLOR}[OFFLINE]${ENDCOLOR}"
+        echo -e "`printf "%-100s\n" check netwrok-cvk-agent service redis status` ${REDCOLOR}[OFFLINE]${ENDCOLOR}"
+        return 1
+    elif [ -z "`tail -f -n 200 /var/log/network-cvk-agent/network-cvk-agent.log | grep "ping redis failed"`"];then
+        echo -e "`printf "%-100s\n" check netwrok-cvk-agent service redis status` ${REDCOLOR}[OFFLINE]${ENDCOLOR}"
+        return 1
     fi
-    REDISMODE=`cat /etc/network-cvk-agent/config.json | grep -w mode | awk -F '"' '{print $4}'`
+
+    echo -e "`printf "%-100s\n" check netwrok-cvk-agent config.json` ${GREENCOLOR}[OK]${ENDCOLOR}"
 }
 
+#1、检查网络服务状态
 echo "=====================检查网络组件状态========================"
 
 funcServiceStatus "network.service"
@@ -58,7 +88,10 @@ if [ -f ${CONFIGPATH} ];then
     funcIsEmpty ${AGENT_VTEP_IP} "vtepip"
     funcIsEmpty ${AGENT_AZ_ID} "az_id"
     funcIsEmpty ${AGENT_BGP_AS} "bgpas"
-    
+
+    funcCheckRedis
+
+
 else
     echo -e "network-cvk-agent.service                                          ${REDCOLOR}[/etc/network-cvk-agent/config.json miss]${ENDCOLOR}"
 fi
